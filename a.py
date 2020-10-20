c20fd8444e66ba9da7d56e703ff5fada2fd7cb8d
LIE9707 <lile@bupt.edu.cn>
2020-10-20 12:54:33 +0800

    啊啊

--- a.py
+++ a.py
@@ -19,12 +19,25 @@ import re
 
 # print(list(sources))
 
-from dingtalkchatbot.chatbot import DingtalkChatbot
+from dingtalkchatbot.chatbot import DingtalkChatbot,CardItem
 webhook = 'https://oapi.dingtalk.com/robot/send?access_token=81355aec2d36297d67978f3db1fe8d54819cb498bae833d0ad87dc4a6bb1237e'		# WebHook地址
-xiaoding = DingtalkChatbot(webhook)		# 初始化机器人小丁
+secret = 'SEC31ae94c1e65c90cea577ce5b390412e1b5b6c3da7271e53f5fe1df52b6d89d05'
+xiaoding = DingtalkChatbot(webhook, secret=secret)		# 初始化机器人小丁
 
 #发送文字消息
-xiaoding.send_text(msg='通知：2020-10-20 09:13:54', is_at_all=True)
+xiaoding.send_text(msg='爬取了200条新闻，过滤20条', is_at_all=False)
+
+# cards=[]
+# card1 = CardItem(title='中国儿女', url='http://baidu,com', pic_url="https://i.loli.net/2020/10/20/hHPAjzSfZCceYvX.jpg")
+# card2=CardItem(title='中国儿女', url='http://baidu,com', pic_url="https://i.loli.net/2020/10/20/hHPAjzSfZCceYvX.jpg")
+# cards.append(card1)
+# cards.append(card2)
+
+# # xiaoding.send_text(msg=f'通知：{reason}\n'+t, is_at_all=True)
+# # FeedCard消息类型
+# xiaoding.send_feed_card(cards)
 
 
-
+# a = '123123'
+# b = f'sadkfjaslkdjfklsdfj{a}alksdjf'
+# print(b)
